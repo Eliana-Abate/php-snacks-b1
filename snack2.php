42da5ef112b49e7f4e0250172eb0509cca39e29a
Eliana-Abate <eliana.abate@outlook.it>
2021-09-23 18:20:16 +0200

    added array $class

--- snack2.php
+++ snack2.php
@@ -1,3 +1,35 @@
+<!-- SNACK 2
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici (senza, materia, solo numeri). 
+Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php 
+
+    $class = [
+        ['name' => 'John', 
+        'surname' => 'Doe', 
+        'subject' => ['7', '6', '3', '8', '5']
+        ],
+
+        ['name' => 'Alice', 
+        'surname' => 'Smith', 
+        'subject' => ['6', '6', '7', '6', '8']
+        ],
+
+        ['name' => 'Walter', 
+        'surname' => 'White', 
+        'subject' => ['4', '7', '6', '5', '5']
+        ],
+
+        ['name' => 'Jessica', 
+        'surname' => 'Brown', 
+        'subject' => ['8', '9', '7', '9', '7']
+        ],
+    ]
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
