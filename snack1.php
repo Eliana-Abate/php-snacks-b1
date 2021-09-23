9e8c79c8807aa96e58a555590d9dc85f15636e2a
Eliana-Abate <eliana.abate@outlook.it>
2021-09-23 18:03:09 +0200

    added comment

--- snack1.php
+++ snack1.php
@@ -1,3 +1,8 @@
+<!-- SNACK 1
+Creare una funzione che restituisce un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero 
+più di una volta. Stampare i numeri in pagina. -->
+
+
 <?php 
     function get_random_number($min, $max, $items) {
     
